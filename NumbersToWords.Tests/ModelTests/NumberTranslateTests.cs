f7f052ca8f2cb0247bc39b7972620de9fcaacfdc
Ben White <bwhite2140@outlook.com>
2020-07-16 15:50:24 -0700

    add passing test for tens dictionary
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- NumbersToWords.Tests/ModelTests/NumberTranslateTests.cs
+++ NumbersToWords.Tests/ModelTests/NumberTranslateTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NumbersToWords.Models;
+using System.Collections.Generic;
 
 namespace NumbersToWords.Tests
 {
@@ -10,6 +11,47 @@ namespace NumbersToWords.Tests
     public void IsNumber_CheckIfUserEnteredANumber_Bool()
     {
       Assert.AreEqual(true, NumberTranslate.IsNumber());
+    }
+
+    [TestMethod]
+    public void TranslateOnes_ConvertOnesToEnglish_Dictionary()
+    {
+      Dictionary<int, string> ones = new Dictionary<int, string> {
+        {0, "Zero"},
+        {1, "One"}, 
+        {2, "Two"}, 
+        {3, "Three"}, 
+        {4, "Four"},
+        {5, "Five"},
+        {6, "Six"},
+        {7, "Seven"},
+        {8, "Eight"},
+        {9, "Nine"}};
+      CollectionAssert.AreEqual(ones, NumberTranslate.TranslateOnes());
+    }
+    [TestMethod]
+    public void TranslateTens_ConvertTensToEnglish_Dictionary()
+    {
+      Dictionary<int, string> tens = new Dictionary<int, string> {
+        {10, "Ten"},
+        {11, "Eleven"}, 
+        {12, "Twelve"}, 
+        {13, "Thirteen"}, 
+        {14, "Fourteen"},
+        {15, "Fifteen"},
+        {16, "Sixteen"},
+        {17, "Seventeen"},
+        {18, "Eighteen"},
+        {19, "Nineteen"},
+        {20, "Twenty"},
+        {30, "Thirty"},
+        {40, "Forty"},
+        {50, "Fifty"},
+        {60, "Sixty"},
+        {70, "Seventy"},
+        {80, "Eighty"},
+        {90, "Ninety"}};
+      CollectionAssert.AreEqual(tens, NumberTranslate.TranslateTens());
     }
   }
 }
--- NumbersToWords/Models/NumberTranslate.cs
+++ NumbersToWords/Models/NumberTranslate.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace NumbersToWords.Models
 {
   public class NumberTranslate
@@ -10,6 +12,46 @@ namespace NumbersToWords.Models
         return true;
       }
       return false;
+    }
+
+    public static Dictionary<int, string> TranslateOnes()
+    {
+      Dictionary<int, string> onesColumn = new Dictionary<int, string> {
+        {0, "Zero"},
+        {1, "One"}, 
+        {2, "Two"}, 
+        {3, "Three"}, 
+        {4, "Four"},
+        {5, "Five"},
+        {6, "Six"},
+        {7, "Seven"},
+        {8, "Eight"},
+        {9, "Nine"}};
+      return onesColumn;
+    }
+
+    public static Dictionary<int, string> TranslateTens()
+    {
+      Dictionary<int, string> tensColumn = new Dictionary<int, string> {
+        {10, "Ten"},
+        {11, "Eleven"}, 
+        {12, "Twelve"}, 
+        {13, "Thirteen"}, 
+        {14, "Fourteen"},
+        {15, "Fifteen"},
+        {16, "Sixteen"},
+        {17, "Seventeen"},
+        {18, "Eighteen"},
+        {19, "Nineteen"},
+        {20, "Twenty"},
+        {30, "Thirty"},
+        {40, "Forty"},
+        {50, "Fifty"},
+        {60, "Sixty"},
+        {70, "Seventy"},
+        {80, "Eighty"},
+        {90, "Ninety"}};
+      return tensColumn;
     }
   }
 }
